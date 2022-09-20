334da2240d6955f4576178b995378116892e5f83
TheStickLord <87539497+TheStickLord@users.noreply.github.com>
2022-09-20 16:23:01 -0400

    Update transition.js

--- scripts/transition.js
+++ scripts/transition.js
@@ -2,12 +2,12 @@
 
 setTimeout(() => {
   document.querySelector(".loadTextContent").textContent = "Loading Page...";
-}, 7500);
+}, 6000);
 
 setTimeout(() => {
   document.querySelector(".loadTextContent").textContent = "Done!";
-}, 12000);
+}, 8000);
 
 setTimeout(() => {
   window.location.replace("main.html");
-}, 15000);
+}, 10000);
